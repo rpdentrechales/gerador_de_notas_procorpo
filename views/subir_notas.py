cc8e9b3eaf9c1bdfb3f184004af974d7ea605921
rpdentrechales <rpdprocorpo@gmail.com>
2024-10-23 13:52:52 +0000

    Updating files

--- views/subir_notas.py
+++ views/subir_notas.py
@@ -6,25 +6,7 @@ from auxiliar.auxiliar import load_main_dataframe
 
 st.set_page_config(page_title="Subir Notas", page_icon="💎",layout="wide")
 
-# @st.cache_data
-# def load_main_dataframe(worksheet):
-
-#   conn = st.connection("gsheets", type=GSheetsConnection)
-#   df = conn.read(worksheet=worksheet,dtype={"Ad ID": str})
-
-#   return df
-
-# @st.cache_data
-# def load_aux_dataframe(worksheet,duplicates_subset):
-
-#   conn = st.connection("gsheets", type=GSheetsConnection)
-#   df = conn.read(worksheet=worksheet)
-#   df = df.drop_duplicates(subset=duplicates_subset)
-
-#   return df
-
 st.title("Subir Notas - Teste")
-
 
 df = load_main_dataframe("CRM - Billcharges (Json)")
 st.dataframe(df)
