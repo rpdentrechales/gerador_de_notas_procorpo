75a10534c73a7783f2b398197eeca1faff6dbf0e
rpdentrechales <rpdprocorpo@gmail.com>
2024-10-23 15:30:49 +0000

    Updating files

--- views/subir_notas.py
+++ views/subir_notas.py
@@ -12,7 +12,7 @@ st.title("Subir Notas - Testar Billcharge")
 today = datetime.datetime.now()
 three_days_ago = today - timedelta(days=3)
 
-col_data_1, col_data_2, blank_3 = st.columns([1.5,1,2])
+col_data_1, col_data_2, blank_3 = st.columns([1,1,3])
 
 with col_data_1:
   data_seletor = st.date_input(
