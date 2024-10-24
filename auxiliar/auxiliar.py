b2861528081fbfd8c3a37cdde58225b0e52cf4be
rpdentrechales <rpdprocorpo@gmail.com>
2024-10-24 15:26:55 +0000

    Updating files

--- auxiliar/auxiliar.py
+++ auxiliar/auxiliar.py
@@ -443,7 +443,7 @@ def criar_clientes_selecionados(base_df):
   resultados = [["client_id","Resultado","Response"]]
   counter = 0
 
-  for row in base_df:
+  for indes,row in base_df.iterrows():
   
     dados_cliente = row["dados_cliente"]
     unidade = row["store_name"]
