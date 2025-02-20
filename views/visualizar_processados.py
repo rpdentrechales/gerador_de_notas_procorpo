fa6534bc5d5dbeeefae4764e7cdf7de9fd84609a
cironis <ciroshia@gmail.com>
2025-02-20 17:46:05 -0300

    update

--- views/visualizar_processados.py
+++ views/visualizar_processados.py
@@ -8,21 +8,6 @@ st.set_page_config(page_title="OS Processadas", page_icon="💎",layout="wide")
 
 st.title("OS Processadas")
 
-today = datetime.datetime.now()
-three_days_ago = today - timedelta(days=3)
+os_processados = pegar_dados_mongodb("os_processados")
 
-col_data_1, col_data_2, blank_3 = st.columns([1,1,2])
-
-with col_data_1:
-  data_seletor = st.date_input(
-      "Selecione a data",
-      (three_days_ago, today),
-      format="DD/MM/YYYY",
-  )
-
-  if len(data_seletor) == 2:
-    data_inicial = data_seletor[0].strftime('%Y-%m-%d')
-    data_final = data_seletor[1].strftime('%Y-%m-%d')
-  else:
-    data_inicial = data_seletor[0].strftime('%Y-%m-%d')
-    data_final = data_inicial
+st.dataframe(os_processados)
