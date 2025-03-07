8a5e6d00a46f9e062e6bf4601149a6529f36c16c
cironis <ciroshia@gmail.com>
2025-03-07 15:42:49 -0300

    update

--- auxiliar/auxiliar.py
+++ auxiliar/auxiliar.py
@@ -741,3 +741,18 @@ def atualizar_base_de_clientes():
   codigo_integracao_omie_mongodb = pegar_dados_mongodb("id_clientes")
 
   return (codigo_integracao_omie_mongodb,todos_dados_clientes)
+
+
+def deletar_todos_documentos(collection_name, query=None):
+    client = MongoClient("mongodb+srv://rpdprocorpo:iyiawsSCfCsuAzOb@cluster0.lu6ce.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+    db = client["notas_omie"]
+    collection = db[collection_name]
+    
+    # Delete all documents if no query is specified
+    if query is None:
+        result = collection.delete_many({})
+    else:
+        result = collection.delete_many(query)
+    
+    print(f"Documentos deletados: {result.deleted_count}")
+    client.close()
