63cefb00d4289c1ffae5fbd0377699efaa7fac5c
cironis <ciroshia@gmail.com>
2025-03-12 11:47:06 -0300

    update

---
+++ auxiliar/omie_aux.py
@@ -0,0 +1,13 @@
+import streamlit as st
+import pandas as pd
+import datetime
+from streamlit_gsheets import GSheetsConnection
+import requests
+import json
+import re
+import time
+import pymongo
+from pymongo import MongoClient
+import numpy as np
+
+
