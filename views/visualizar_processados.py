e4079c0a56013487db09f2c3ee23c1d7eb35b12d
rpdentrechales <rpdprocorpo@gmail.com>
2025-05-27 14:49:13 -0300

    update

--- views/visualizar_processados.py
+++ views/visualizar_processados.py
@@ -30,11 +30,11 @@ if pegar_os_botao:
     st.write(f"Buscando OS processadas entre {data_inicial} e {data_final}...")
     os_processados = criar_dataframe_os(data_inicial, data_final)
     os_processados["deletar"] = "Sim"
-    
+
     st.data_editor(
         os_processados,
         column_config={
-            "id_os": st.column_config.TextColumn("Id OS", format="%d"),
+            "id_os": st.column_config.TextColumn("Id OS"),
             "data_faturamento": st.column_config.TextColumn("Data de Faturamento"),
             "valor_total": st.column_config.NumberColumn("Valor", format="R$ %.2f"),
             "unidade": st.column_config.TextColumn("Unidade"),
